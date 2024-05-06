a2fd9d9f0fe54f46beebf3e0c51c26a38701307b
mileprogramer <obecano.ispunjeno@gmail.com>
2024-05-06 17:36:02 +0200

    Adding best selling functionlity

--- src/pages/BestSellingCars.Page.js
+++ src/pages/BestSellingCars.Page.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Navbar from "../components/Navbar.Component";
+import carService from "../services/carService";
 
 function BestSellingCars(props) {
+
+    const [cars, setBestSellingCars] = useState([]);
+
+    useEffect(() => {
+        async function fetchBestSellingCars(){
+            let cars = await carService.bestSellingCars(3);
+            setBestSellingCars(cars);
+        }
+
+        fetchBestSellingCars();
+    }, []);
+
     return (
         <>
         <Navbar/>
         <div className="container">
-            <div className="row">
-                <div className="card col-3">
-                    <div className="card-header">
-                        Merecedes Benz 2006
-                    </div>
-                </div>
+            <div className="row justify-content-center gap-3">
+                {
+                    cars.map((car, index)=> {
+                        return <div key={index} className="card col-3">
+                            <div className="card-header">
+                                License: {car.license}
+                            </div>
+                            <div className="car-body">
+                                Brand: {car.brand}<br/>
+                                Model: {car.model}
+                            </div>
+                        </div>
+                    })
+                }
             </div>
         </div>
         </>
