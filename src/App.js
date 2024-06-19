44d66fcbf4983b06de1d73c423a70d5537b8c9ec
mileprogramer <obecano.ispunjeno@gmail.com>
2024-06-19 21:54:16 +0200

    Add route to the page history rented cars

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import BestSellingCars from "./pages/BestSellingCars.Page";
 import AddCar from "./pages/AddCar.Page";
 import EditDeletePage from "./pages/EditDelete.Page";
 import RentedCarsPage from "./pages/RentedCars.Page";
+import HistoryRentedCars from "./pages/HistoryRentedCars.Page";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Routes>
                 <Route exact path="/" element={<HomePage />} />
                 <Route exact path="/rented-cars" element={<RentedCarsPage />} />
+                <Route exact path="/history-rented-cars" element={<HistoryRentedCars />} />
                 <Route path="/best-selling-cars" element={<BestSellingCars />} />
                 <Route path="/add-car" element={<AddCar />} />
                 <Route path="/edit-delete-cars" element={<EditDeletePage />} />
