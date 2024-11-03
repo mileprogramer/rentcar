3f1f271c78279604b1c4c0c7d322570ebb8c2a04
mileprogramer <obecano.ispunjeno@gmail.com>
2024-11-03 15:58:23 +0100

    refactor the the search dropdown

--- src/components/SearchableDropDown.Component.jsx
+++ src/components/SearchableDropDown.Component.jsx
@@ -1,107 +1,70 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
+import Mistake from "../components/MistakesAlert.Component"
 import "../css/drop-down.css"
 
-const SearchableDropdown = ({ options, search, getData ,inputLabel, label, additionalLabel = null, validationErrors = [], id, selectedVal, handleChange }) => {
+export default function SearchableDropDown({ options, selectOption, renderName, resetSearch,  setSearchTerm, validationErrors, isLoading, inputLabel }) {
+    
+    const [inputValue, setInputValue] = useState("");
+    const [isOpen, setIsOpen] = useState("");
 
-    const [query, setQuery] = useState("");
-    const [isOpen, setIsOpen] = useState(false);
-
-    const inputRef = useRef(null);
-
-    useEffect(() => {
-        document.addEventListener("click", toggle);
-        let searchableDropDown = document.querySelector(".dropdown input")
-        searchableDropDown.addEventListener("input", toggle);
-        return () =>{
-            searchableDropDown.addEventListener("input", toggle);
-            document.removeEventListener("click", toggle);
-        };
-    }, []);
-
-    useEffect(()=>{
-        if(!query){
-            getData();
+    const handleSearch = (e) => {
+        setInputValue(e.target.value)
+        if(e.target.value === "") {
+            setIsOpen(false)
+            resetSearch();
         }
-    }, [query])
+        setSearchTerm(e.target.value);
+        setIsOpen(true);
 
-    const selectOption = (option) => {
-        setQuery(() => "");
-        handleChange(option);
-        setIsOpen((isOpen) => !isOpen);
-    };
-
-    function toggle(e) {
-        setIsOpen(e && e.target === inputRef.current);
     }
 
-    const getDisplayValue = () => {
-        if (query) return query;
-        if (selectedVal) return selectedVal;
+    function displayOptions() {
 
-        return "";
-    };
-
-    const filter = (options) => {
-        let results = options.filter(
-            (option) => option[label].toLowerCase().indexOf(query.toLowerCase()) > -1 || option[additionalLabel].toLowerCase().indexOf(query.toLowerCase()) > -1
-        );
-        if(results.length === 0){
-            search(query);
+        if(!Array.isArray(options)) {
+            return "";
         }
-        return results;
-    };
 
-    return (
-    <>
+        if(options.length > 0) {
+            return options.map((option, index) => {
+                return (
+                    <div
+                        onClick={() => {
+                            selectOption(option)
+                            setInputValue(renderName(option))
+                            setIsOpen(false);
+                            resetSearch();
+                        }}
+                        className={`option`}
+                        key={`${index}`}
+                    >
+                        {renderName(option)}
+                    </div>
+                );
+            })
+        }
+        else if(options.length === 0 && !isLoading) {
+            return <div className="no-option">There is not result for this search</div> 
+        }
+        return "";
+    }
+
+    return <>
         <div className="dropdown">
             <label htmlFor="text">{inputLabel}</label>
             <div className="control">
                 <div className="selected-value">
                     <input
-                        ref={inputRef}
-                        type="text"
-                        value={getDisplayValue()}
+                        type="input"
+                        value={inputValue}
                         name="searchTerm"
-                        onChange={(e) => {
-                            setQuery(e.target.value);
-                            handleChange(null);
-                        }}
-                        onClick={toggle}
+                        onChange = {handleSearch}
                     />
                 </div>
-            <div className={`arrow ${isOpen ? "open" : ""}`}></div>
+                <div className={`arrow ${isOpen ? "open" : ""}`}></div>
             </div>
 
-            <div className={`options ${isOpen ? "open" : ""}`}>
-            {filter(options).map((option, index) => {
-                return (
-                <div
-                    onClick={() => selectOption(option)}
-                    className={`option ${
-                    option[label] === selectedVal ? "selected" : ""
-                    }`}
-                    key={`${id}-${index}`}
-                >
-                    {
-                        additionalLabel ?
-                        option[label] + "-" + option[additionalLabel] : option[label]
-                    }
-                </div>
-                );
-            })}
-            </div>
+            <div className={`options ${isOpen ? "open" : ""}`}> {displayOptions()} </div>
+            <Mistake mistakes = {validationErrors} />
         </div>
-        {
-            validationErrors.length !== 0 ? (
-                <ul>
-                    {validationErrors.map((el, index) => (
-                        <li className="text-danger" key={index}>{el}</li>
-                    ))}
-                </ul>
-            ) : ""
-        }
     </>
-    );
-};
-
-export default SearchableDropdown;
+}
